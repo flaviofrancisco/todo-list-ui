d4062f7f263472ee55ccb9cd319937c233c10161
Flavio Francisco <flaviofrancis@msn.com>
2020-07-18 11:46:24 +0200

    config.ts removed from .gitignore

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '4ju40fur6i'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
+
+export const authConfig = {  
+  domain: 'ffr.eu.auth0.com',            
+  clientId: 'C1d2tpLBBJe5pmjf3bdwgAa386DIpnO0',
+  callbackUrl: 'http://localhost:3000/callback'
+}
